6085a1f28187ec832ead0ca9705670d8221256bd
Mahad Munir <mahadmunir10@gmail.com>
2022-02-14 17:35:27 +0500

    check jsonObj & jsonArray

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"strconv"
@@ -17,6 +18,25 @@ func isNumeric(value string) (int, error) {
 func isBoolean(value string) (bool, error) {
 	return strconv.ParseBool(value)
 }
+func isJsonObj(value string)(bool, map[string]interface{}){
+	var jsonMap map[string]interface{}
+	json.Unmarshal([]byte(value), &jsonMap)
+	if jsonMap != nil{
+		return true, jsonMap
+	}
+	return false, jsonMap
+}
+
+
+func isJsonArray(value string)(bool, [] interface{}){
+	var jsonArr [] interface{}
+	json.Unmarshal([]byte(value), &jsonArr)
+	if jsonArr != nil{
+		return true, jsonArr
+	}
+	return false, jsonArr
+}
+
 
 func covertIntoDataType(listToConvert []string) interface{} {
 	var arr1 []interface{}
@@ -36,16 +56,25 @@ func covertIntoDataType(listToConvert []string) interface{} {
 			arr1 = append(arr1, boolVal)
 			continue
 		}
+		var jsonMap map[string]interface{}
+		checkJsonObj, jsonMap := isJsonObj(value)
+		if checkJsonObj {
+			arr1 = append(arr1, jsonMap)
+			continue
+		}
+		checkJsonArr, jsonArr := isJsonArray(value)
+		if checkJsonArr{
+			arr1 = append(arr1, jsonArr)
+			continue
+		}
 		arr1 = append(arr1, value)
 	}
 	return arr1
 }
 
 func main() {
-	readers := []string{"4.0", "Alice", "False", "Charlie"}
-	readers = append(readers, "42")
-	readers = append(readers, "0.3")
-	readers = append(readers, "True")
+	readers := []string{"4.0", "Alice", "False", "Charlie", "42", "0.3", "True", "{\"data\": \"key\"}",
+		"[{\"data\":\"key\"}]", "[\"Hello world\", 10, false]"}
 	fmt.Println("List values are: ", readers)
 	arr1 := covertIntoDataType(readers)
 	for _, value := range arr1.([]interface{}) {
