52c5393a852ff5ee7f3ba038f63d6a5e9f77c14c
Mahad Munir <mahadmunir10@gmail.com>
2022-02-14 14:14:22 +0500

    code clean

--- main.go
+++ main.go
@@ -6,36 +6,6 @@ import (
 	"strconv"
 )
 
-//func isIntegral(val float64) bool {
-//	return val == float64(int(val))
-//}
-//
-//func isFloatint(floatValue float64) bool {
-//	return math.Mod(floatValue, 1.0) == 0
-//}
-//
-//func isFloatInt(value string) interface{} {
-//	val := strings.Split(value, ".")
-//	fmt.Println("Length of val is ", len(val))
-//	if len(val) > 1 {
-//		fmt.Println("Value in float true and value is", value)
-//		floatVal, err := strconv.ParseFloat(value, 64)
-//		fmt.Println("Float val is ", floatVal, " and err is ", err)
-//		return floatVal
-//	}
-//	intVal, _ := strconv.Atoi(value)
-//	return intVal
-//}
-//
-//func isNumeric1(value string) bool {
-//	for _, c := range value {
-//		if c < '0' || c > '9' {
-//			return false
-//		}
-//	}
-//	return true
-//}
-
 func isFloat(value string) (float64, error) {
 	return strconv.ParseFloat(value, 64)
 }
